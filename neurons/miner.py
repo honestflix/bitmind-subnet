97241905768e686339a9e5ffbf60121ddc3c4dfb
Dylan Uys <dylan.uys@gmail.com>
2024-05-01 18:44:05 -0700

    replacing dummy synapse with ImageSynapse

--- neurons/miner.py
+++ neurons/miner.py
@@ -40,7 +40,6 @@ class Miner(BaseMinerNeuron):
 
     def __init__(self, config=None):
         super(Miner, self).__init__(config=config)
-
         # TODO: prep miner model(s)
         # TODO: what else?
 
@@ -64,7 +63,7 @@ class Miner(BaseMinerNeuron):
         return synapse
 
     async def blacklist(
-        self, synapse: template.protocol.Dummy
+        self, synapse: ImageSynapse
     ) -> typing.Tuple[bool, str]:
         """
         Determines whether an incoming request should be blacklisted and thus ignored. Your implementation should
@@ -120,7 +119,7 @@ class Miner(BaseMinerNeuron):
         )
         return False, "Hotkey recognized!"
 
-    async def priority(self, synapse: template.protocol.Dummy) -> float:
+    async def priority(self, synapse: ImageSynapse) -> float:
         """
         The priority function determines the order in which requests are handled. More valuable or higher-priority
         requests are processed before others. You should design your own priority mechanism with care.
