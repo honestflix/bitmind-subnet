f428f1b28036fe2530c8aeee87d3637efb09f4d4
Dylan Uys <dylan.uys@gmail.com>
2024-05-02 22:57:33 -0700

    new readme,

--- validator.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [
-    {
-      name: 'validator',
-      script: 'python3',
-      args: './neurons/validator.py --netuid 93 --logging.debug --logging.trace --subtensor.network local --wallet.name dwalle --wallet.hotkey dwalle_hot''
-    },
-  ],
-};
