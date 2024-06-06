d85335d4b0dc0c1c63de0a33806507d000f72317
Dylan Uys <dylan.uys@gmail.com>
2024-06-06 00:16:15 +0000

    fixing my initial fix for handling single nonzero weight

--- bitmind/base/utils/weight_utils.py
+++ bitmind/base/utils/weight_utils.py
@@ -140,8 +140,9 @@ def process_weights_for_netuid(
 
     # Find all non zero weights.
     non_zero_weight_idx = np.argwhere(weights > 0).squeeze()
-    if not isinstance(non_zero_weight_idx, np.ndarray):
-        non_zero_weight_idx = np.array([non_zero_weight_idx])
+    if non_zero_weight_idx.ndim == 0:
+        non_zero_weight_idx = non_zero_weight_idx.reshape((1,))
+
     non_zero_weight_uids = uids[non_zero_weight_idx]
     non_zero_weights = weights[non_zero_weight_idx]
 
